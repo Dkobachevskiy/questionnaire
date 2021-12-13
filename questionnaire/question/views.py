79a03f42399b24951fbb2ebc0635d472c8fe6a28
Кобачевский Денис <kobacevskiydenis@MacBook-Air-Kobacevskij.local>
2021-12-13 20:16:37 +0300

    new coorections and templates

--- questionnaire/question/views.py
+++ questionnaire/question/views.py
@@ -1,10 +1,28 @@
+from django.http import HttpResponse
 from django.shortcuts import get_list_or_404, render
 from .models import Group, Question
 
 
 def index(request):
-    questions = Question.objects.all
+    questions = Question.objects.all()
     context = {
         'questions' : questions,
     }
     return render(request, 'index.html', context)    
+
+def new_answer(request):
+    form = PostForm(request.POST, files=request.FILES or None)
+    if request.method != 'POST':
+        return render(request, 'index.html', {'form':form})
+    if form.is_valid():
+        post_get = form.save(commit=False)
+        post_get.save()
+        return redirect('thanks')
+    return render(request, 'new.html', {'form':form})
+
+def showthanks(request):
+    message = 'Thanks for your answer'
+    context = {
+        'thanks' : message
+    }
+    return render(request, 'thanks.html', context)
